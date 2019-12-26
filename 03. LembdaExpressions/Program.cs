81d2d28ae19662d618186adc39b8f76c94b57095
T O <tariqaliomar@hotmail.com>
2019-12-26 16:48:54 -0500

    Added ForEach

--- 03. LembdaExpressions/Program.cs
+++ 03. LembdaExpressions/Program.cs
@@ -12,6 +12,8 @@ namespace _03.LembdaExpressions
         { 
             List<int> numbers = new List<int> {5,7,10,15,12,13,255,123,32,67,43,24 };
 
+            numbers.ForEach(p => Console.WriteLine($"{p}"));
+
             //List<int> oddNumbers = (from n in numbers where (n % 2) > 0 select n).ToList();
             List<int> oddNumbers = numbers.Where(n => (n % 2 > 0)).ToList();
 
@@ -29,14 +31,14 @@ namespace _03.LembdaExpressions
             Console.WriteLine($"Average : {avgNumber}, Min : {minNumber}, Max : {maxNumber}");
             Console.WriteLine($"Ordered  :{string.Join(", ", orderedNumbers)}");
             Console.WriteLine($"Greater  :{string.Join(", ", greaterOrderedNumbers)}");
-
-
+            
             Pet[] pets = { new Pet { Name="Barley", Age=8 },
                    new Pet { Name="Boots", Age=4 },
                    new Pet { Name="Whiskers", Age=1 } };
             
             var orderedPets = pets.OrderBy(pet => pet.Age).Select(p => p.Name);
             Console.WriteLine($"Pets  :{string.Join(", ", orderedPets)}");
+                      
 
             Console.WriteLine("Press any key to continue ...");
             Console.ReadKey();
